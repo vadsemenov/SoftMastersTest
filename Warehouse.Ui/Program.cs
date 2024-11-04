32442fa9e96dcb8676c61433486f7e3b69ae7ff7
Vadim Semenov <semenov@bimdance.io>
2024-11-04 20:28:34 +0300

    Add Radzen Blazor

--- Warehouse.Ui/Program.cs
+++ Warehouse.Ui/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using Warehouse.Ui;
 
 const string webApiUrl = "https://localhost:7213/";
@@ -9,5 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(webApiUrl)});
+
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
